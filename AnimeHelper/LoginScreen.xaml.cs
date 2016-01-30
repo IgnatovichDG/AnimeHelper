343d87f6db3de21d07e4816fd1a16ce2702a2784
IgnatovichDG <ignatovich.d.g@yandex.ru>
2016-01-30 03:07:59 +0300

    Log Screen Creater

--- AnimeHelper/LoginScreen.xaml.cs
+++ AnimeHelper/LoginScreen.xaml.cs
@@ -10,19 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AnimeHelper
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginScreen : Window
     {
-        public MainWindow()
+        public LoginScreen()
         {
-            InitializeComponent();           
+            InitializeComponent();
         }
     }
 }
